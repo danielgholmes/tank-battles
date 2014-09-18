7d08c758218e0d072419e357473b9197ceaa1278
danielgholmes <flyholmes@gmail.com>
2014-09-18 16:15:33 +0200

    Start of unit test for collision manager
    Only outlines, not much else

--- source-code/google-tests/ClassTests.cpp
+++ source-code/google-tests/ClassTests.cpp
@@ -1,84 +1,142 @@
 /**
- * \file 	TankTest.cpp
+ * \file 	ClassTests.cpp
  * \author 	Daniel Holmes & Jonathan Gerrand
- * \date 	2 September 2014
- * \brief 	Unit test for Tank class
- *
+ * \date 	18 September 2014
+ * \brief 	Unit tests for all classes. Included in single file for regression testing purposes.
  */
 
 #include <gtest/gtest.h>
+#include "Manager.h"
+#include "CollisionManager.h"
+
 #include "Tank.h"
 #include "Missile.h"
 
-/*================Tests for Tank Class============*/
-TEST(Tank, ifInvalidCoOrdinatesThrowsException){
+/*NOTE: All exceptions are incomplete and commented out*/
+
+/*================Tests for CollisionManager Class============*/
+///addNewEntity
+//TEST(CollisionManager, ifInvalidPointerTypeGivenThrowsException)
+//{
+//
+//}
+//
+//TEST(CollisionManager, ifInvalidPointerObjectGivenThrowsException)
+//{
+//
+//}
+
+///isCollision
+//TEST(CollisionManager, ifInvalidRectangleGivenThrowsException)
+//{
+//
+//}
+
+TEST(CollisionManager, returnsCorrectCollisionOutcome)
+{
 
 }
 
-TEST(Tank, returnsCorrecctEntityType){
+///isOverlap
+//TEST(CollisionManager, ifInvalidAxisGivenThrowsException)
+//{
+//
+//}
+//
+//TEST(CollisionManager, ifInvalidRectangleGivenThrowsException)
+//{
+//
+//}
+
+TEST(CollisionManager, returnsCorrectOverlapOutcome)
+{
 
 }
 
-TEST(Tank, movesForwardCorrectly){
+///calculateAllProjections
+TEST(CollisionManager, )
 
-}
-
-TEST(Tank, movesBackwardCorrectly){
-
-}
-
-TEST(Tank, rotatesLeftCorrectly){
-
-}
-
-TEST(Tank, rotatesRightCorrectly){
-
-}
-
-TEST(Tank, CollidableStateCorrectlyReadAndSet){
-
-}
-
-TEST(Tank, BlockableStateCorrectlyReadAndSet){
-
-}
-/*======================================================*/
-
-
-
-/*================Tests for Missile Class============*/
-TEST(Missile, ifInvalidCoOrdinatesThrowsException){
-
-}
-
-TEST(Missile, returnsCorrecctEntityType){
-
-}
-
-TEST(Missile, movesForwardCorrectly){
-
-}
-
-TEST(Missile, movesBackwardCorrectly){
-
-}
-
-TEST(Missile, rotatesLeftCorrectly){
-
-}
-
-TEST(Missile, rotatesRightCorrectly){
-
-}
-
-TEST(Missile, CollidableStateCorrectlyReadAndSet){
-
-}
-
-TEST(Missile, BlockableStateCorrectlyReadAndSet){
-
-}
-/*======================================================*/
+//*================Tests for Tank Class============*/
+//TEST(Tank, ifInvalidCoOrdinatesThrowsException)
+//{
+//
+//}
+//
+//TEST(Tank, returnsCorrectEntityType)
+//{
+//
+//}
+//
+//TEST(Tank, movesForwardCorrectly)
+//{
+//
+//}
+//
+//TEST(Tank, movesBackwardCorrectly)
+//{
+//
+//}
+//
+//TEST(Tank, rotatesLeftCorrectly)
+//{
+//
+//}
+//
+//TEST(Tank, rotatesRightCorrectly)
+//{
+//
+//}
+//
+//TEST(Tank, CollidableStateCorrectlyReadAndSet)
+//{
+//
+//}
+//
+//TEST(Tank, BlockableStateCorrectlyReadAndSet)
+//{
+//
+//}
+//
+//*================Tests for Missile Class============*/
+//TEST(Missile, ifInvalidCoOrdinatesThrowsException)
+//{
+//
+//}
+//
+//TEST(Missile, returnsCorrectEntityType)
+//{
+//
+//}
+//
+//TEST(Missile, movesForwardCorrectly)
+//{
+//
+//}
+//
+//TEST(Missile, movesBackwardCorrectly)
+//{
+//
+//}
+//
+//TEST(Missile, rotatesLeftCorrectly)
+//{
+//
+//}
+//
+//TEST(Missile, rotatesRightCorrectly)
+//{
+//
+//}
+//
+//TEST(Missile, CollidableStateCorrectlyReadAndSet)
+//{
+//
+//}
+//
+//TEST(Missile, BlockableStateCorrectlyReadAndSet)
+//{
+//
+//}
 
 
 
