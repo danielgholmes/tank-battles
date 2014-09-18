83e1a50f541e00ee9fb8929d7c449ca4c637b41f
danielgholmes <flyholmes@gmail.com>
2014-09-18 14:32:56 +0200

    First implementation of collision algorithm

--- source-code/program/CollisionManager.cpp
+++ source-code/program/CollisionManager.cpp
@@ -42,6 +42,57 @@ void CollisionManager::manage()
 	}
 }
 
+///Algorithm based on the separating axis theorem
+bool CollisionManager::isCollision(rect_corners rect_A, rect_corners rect_B)
+{
+    coordinate axis1;
+    coordinate axis2;
+    coordinate axis3;
+    coordinate axis4;
+
+    // Determine axis for projecting vertices onto
+    axis1.x = rect_A.upper_right.x - rect_A.upper_left.x;
+    axis1.y = rect_A.upper_right.y - rect_a.upper_left.y;
+    axis2.x = rect_A.upper_right.x - rect_A.upper_left.x;
+    axis2.y = rect_A.upper_right.y - rect_A.upper_left.y;
+    axis3.x = rect_B.upper_right.x - rect_B.upper_left.x;
+    axis3.y = rect_B.upper_right.y - rect_B.upper_left.y;
+    axis4.x = rect_B.upper_right.x - rect_B.upper_left.x;
+    axis4.y = rect_B.upper_right.y - rect_B.upper_left.y;
+
+    //need 8 (x,y) points that will give the position of each of the rectangles points projected onto
+    //the axis
+
+    //once we have this, we do the dot product of these points with axis
+
+}
+
+void CollisionManager::projectCornersOntoAxis(coordinate axis, rect_corners rect_A, rect_corners rect_B)
+{
+    std::vector<coordinate> axis_projections;
+    coordinate point;
+
+
+
+    //calculateProjection(projected_vectors_A, rect_A, axis);
+    //calculateProjection(projected_vectors_B, rect_B, axis);
+
+
+}
+
+void CollisionManager::calculateProjection(rect_corners& projected_vectors, rect_corners& rect, coordinate axis)
+{
+    projected_vectors.upper_right.x = ((rect.upper_right.x*axis.x + rect.upper_right.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.x;
+    projected_vectors.upper_right.y = ((rect.upper_right.x*axis.x + rect.upper_right.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.y;
+    projected_vectors.upper_left.x = ((rect.upper_left.x*axis.x + rect.upper_left.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.x;
+    projected_vectors.upper_left.y = ((rect.upper_left.x*axis.x + rect.upper_left.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.y;
+    projected_vectors.lower_right.x = ((rect.lower_right.x*axis.x + rect.lower_right.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.x;
+    projected_vectors.lower_right.y = ((rect.lower_right.x*axis.x + rect.lower_right.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.y;
+    projected_vectors.lower_left.x = ((rect.lower_left.x*axis.x + rect.lower_left.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.x;
+    projected_vectors.lower_left.y = ((rect.lower_left.x*axis.x + rect.lower_left.y*axis.y)/((axis.x)^2 + (axis.y)^2))*axis.y;
+}
+
+
 void CollisionManager::setCollisionStates(std::shared_ptr<Collidable> entity_1, std::shared_ptr<Collidable> entity_2)
 {
 	if ((entity_1->getType() == p1_tank) || (entity_1->getType() == p2_tank))
